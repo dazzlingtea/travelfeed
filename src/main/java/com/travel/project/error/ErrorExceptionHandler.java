062efd1a7e735e64ba82d5c7be9bdd10cee3a51b
dazzlingtea <kitsnr08@gmail.com>
2024-12-07 23:59:32 +0900

    # 컨트롤러 전역 예외처리

---
+++ src/main/java/com/travel/project/error/ErrorExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.travel.project.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("죄송합니다. 다시 시도해주세요. " + e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
